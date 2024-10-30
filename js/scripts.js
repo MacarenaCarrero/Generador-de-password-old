1ac3bf7bc2d5fc4f6cc67b54a5f3a792d24baa7a
MacarenaCarrero <macarena.c.rios@gmail.com>
2024-10-31 00:01:51 +0100

    generador de password

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,26 @@
+
 const rangeElement = document.getElementById("range");
-
-const printValue = (event) => {
-  console.log(event.target.value);
-};
-
 const passwordElement = document.getElementById("password");
-const passworsContent =
+const lengthPassElement = document.getElementById("lengthPass")
+const passwordContent =
   "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz01234567890!@#$%^&*()_+-={}[]:;<>,.?/</>";
+  const buttonPasswordElement = document.getElementById("buttonPassword")
 
-const randomPassword = (num) => {};
 
-rangeElement.addEventListener("input", printValue);
+const lengthPass= (event) => {
+console.log(event.target.value);
+lengthPassElement.textContent ="Length : " + event.target.value
+}
+
+const password = () => {
+  let newPassword =""
+  for (let i =0 ; i  < lengthPass; i++){
+    const randomNumber = Math.floor(Math.random()*passwordContent.length)
+    const randomCharacter = passwordContent.charAt(randomNumber)
+    newPassword += randomCharacter
+  }
+}
+
+
+rangeElement.addEventListener("input",lengthPass);
+buttonPasswordElement.addEventListener("click",password);
