46526c6214b95fd3273977687e3ca4d6c1e8f906
Diegohhallam <macarena.c.rios@gmail.com>
2024-10-30 18:57:35 +0100

    Generador de password

--- js/scripts.js
+++ js/scripts.js
@@ -3,4 +3,11 @@ const rangeElement = document.getElementById("range");
 const printValue = (event) => {
   console.log(event.target.value);
 };
+
+const passwordElement = document.getElementById("password");
+const passworsContent =
+  "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz01234567890!@#$%^&*()_+-={}[]:;<>,.?/</>";
+
+const randomPassword = (num) => {};
+
 rangeElement.addEventListener("input", printValue);
